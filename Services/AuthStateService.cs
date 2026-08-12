6bc38e8c7c5eabf3a7722dac9d1b937bc7e8f1af
RayVenShellhart <rayvenshellhart@gmail.com>
2026-08-11 19:33:17 -0700

    auth, products, and login

--- Services/AuthStateService.cs
+++ Services/AuthStateService.cs
@@ -1,4 +1,5 @@
 using CSE325_Team4_GroupProject.Models;
+using Microsoft.JSInterop;
 
 namespace CSE325_Team4_GroupProject.Services;
 
@@ -9,6 +10,9 @@ namespace CSE325_Team4_GroupProject.Services;
 /// </summary>
 public class AuthStateService
 {
+    private const string UserIdStorageKey = "shophub_user_id";
+
+    private readonly IJSRuntime _jsRuntime;
     private User? _currentUser;
     private event Action? _onChange;
 
@@ -20,18 +24,84 @@ public class AuthStateService
         remove => _onChange -= value;
     }
 
+    public AuthStateService(IJSRuntime jSRuntime)
+    {
+        _jsRuntime = jSRuntime;
+    }
+
+    // Stores user. reme,ber me stores users id in local storage
+    public async Task SetUserAsync(User? user, bool rememberMe = false)
+    {
+        _currentUser = user;
+
+        if (rememberMe && user != null)
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "localStorage.setItem",
+                UserIdStorageKey,
+                user.Id.ToString()
+            );
+        }
+        else
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "localStorage.removeItem",
+                UserIdStorageKey
+            );
+        }
+
+        NotifyStateChanged();
+    }
+
+    //Restores a previous user
+    public async Task RestoreUserAsync(UserService userService)
+    {
+        try
+        {
+            var storedUserId = await _jsRuntime.InvokeAsync<string?>(
+                "localStorage.getItem",
+                UserIdStorageKey
+            );
+
+            if (int.TryParse(storedUserId, out int userId))
+            {
+                var user = await userService.GetUserByIdAsync(userId);
+
+                if (user != null && user.IsActive)
+                {
+                    _currentUser = user;
+                    NotifyStateChanged();
+                    return;
+                }
+            }
+        }
+        catch (JSException)
+        {
+
+        }
+
+        _currentUser = null;
+    }
+
+    public async Task LogoutAsync()
+    {
+        _currentUser = null;
+
+        await _jsRuntime.InvokeVoidAsync(
+            "localStorage.removeItem",
+            UserIdStorageKey
+        );
+
+        NotifyStateChanged();
+    }
+
     public void SetUser(User? user)
     {
         _currentUser = user;
         NotifyStateChanged();
     }
 
-    public void Logout()
-    {
-        _currentUser = null;
-        NotifyStateChanged();
-    }
-
+    // notifies that authentication state has changed 
     private void NotifyStateChanged()
     {
         _onChange?.Invoke();
